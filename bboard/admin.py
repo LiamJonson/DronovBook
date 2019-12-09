e13807c1e054e936349325e4b8bf896f50248879
LiamJonson <Liamjonson@rambler.ru>
2019-12-09 14:47:32 +0300

    Administrative site, editing fields and models in it, as well as changing the standard model to soy through the editor class

--- bboard/admin.py
+++ bboard/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bb
+
+
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')  # имена в списки записей
+    list_display_links = ('title', 'content')                  # в гиперсылки на стр правки записи
+    search_fields = ('title', 'content')                       # по которым выполн фильтр
+
+
+admin.site.register(Bb, BbAdmin)  # регист своего прилож и модели
--- bboard/models.py
+++ bboard/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50)  # допустимая 50 симвл
-    content = models.TextField(null=True, blank=True)  # можно не заполнять,неогран.длинны
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True) # текущ.дат.и врем. и идекс
+    title = models.CharField(max_length=50, verbose_name='Товар')  # допустимая 50 симвл
+    content = models.TextField(null=True, blank=True, verbose_name='Описание')  # можно не заполнять,неогран.длинны
+    price = models.FloatField(null=True, blank=True, verbose_name='Цена')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано') # текущ.дат.и врем. и идекс
+
+    class Meta:
+        verbose_name_plural = 'Обьявления'  #множ.числ
+        verbose_name = 'Обьявление'         #ед.ч
+        ordering =['-published']            #сортировка
